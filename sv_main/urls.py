d54930eadeee3d2fb2bc2eaac9d57495921485db
KengPenro <kengpenro3000.1.0@gmail.com>
2025-10-21 23:56:37 +0300

    lay out home page and style header

--- sv_main/urls.py
+++ sv_main/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("main/", views.mainpage, name="main_page"),
+    path("", views.mainpage, name="main_page"),
     path("poll/", views.poll, name="poll_page"), 
     path("vessels/", views.vessels, name="vessels_page"), 
     path("categories/", views.categories, name="categories_page")
--- sv_main/views.py
+++ sv_main/views.py
@@ -5,11 +5,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def mainpage (request):
-    #template = loader.get_template('templates/mainpage.html')
-<<<<<<< HEAD
-    return HttpResponse('hello world from mainpage view!')
-=======
-    #return HttpResponse('hello world from mainpage view!')
     return render(request, 'main_page.html')
 
 def poll(request):
@@ -20,4 +15,3 @@ def vessels(request):
 
 def categories(request):
     return render(request, "categories_page.html")
->>>>>>> a6a4391 (create new pages, make in them header and footer and connect urls)
