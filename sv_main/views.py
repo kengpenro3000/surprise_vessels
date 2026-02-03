04c81b4490d5de76ad68fc07a94a3d13dfdbeb5e
mxxun <mrgutkun@gmail.com>
2026-02-03 19:49:31 +0300

    Revert "resolved html render trouble"; probably this commit is irrelevant & this way we don't need to resolve its merge conflicts
    This reverts commit 75d203f70e4987313ac25340dda7ce8592db3116.

--- sv_main/views.py
+++ sv_main/views.py
@@ -4,7 +4,6 @@ from django.http import HttpResponse
 
 
 # Create your views here.
-def mainpage(request):
+def mainpage (request):
     #template = loader.get_template('templates/mainpage.html')
-    #return HttpResponse('hello world from mainpage view!')
-    return render(request, 'mainpage.html')
+    return HttpResponse('hello world from mainpage view!')
