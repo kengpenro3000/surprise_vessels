42765feea92e9d049c214d4afc06a0b723a330a5
mimikria300 <mimikria300@gmail.com>
2026-02-06 13:49:00 +0300

    fixed some front

--- sv_main/constants.py
+++ sv_main/constants.py
@@ -2,8 +2,16 @@
 DEFAULT_VESSEL_IMAGE = '/media/vessels/default.jpg'
 DEFAULT_CATEGORY_IMAGE = '/media/vessels_cats/default.jpg'
 
+# DEFAULT_PARAMETERS = {
+#     "злонамеренность" : 0,
+#     "скрытность" : 0,
+#     "интеллект" : 0,
+#     "чувство юмора" : 0,
+#     "пузатость": 0
+# }
+
 DEFAULT_PARAMETERS = {
-    "a" : 0,
-    "b" : 0,
-    "c" : 0
+    "a": 0,
+    "b": 0,
+    "c": 0
 }
